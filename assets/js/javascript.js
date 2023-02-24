6d53f118766fca28b3311f5017734dab11d491a6
David Rodriguez <davidroddriguez896@gmail.com>
2023-02-24 03:01:24 -0600

    Se agrego boton de chat y su funcionalidad

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -29,3 +29,12 @@ for (let i = 0; i < enlaces.length; i++) {
     event.preventDefault();
   });
 }
+
+document .querySelector(".openChatBtn") .addEventListener("click", openForm);
+document.querySelector(".close").addEventListener("click", closeForm);
+function openForm() {
+   document.querySelector(".openChat").style.display = "block";
+}
+function closeForm() {
+   document.querySelector(".openChat").style.display = "none";
+}
